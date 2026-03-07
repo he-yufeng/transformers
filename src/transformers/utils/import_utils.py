b4bd97ba00b9afe136f2962b7c6f67fdc6070d6e
ydshieh <ydshieh@users.noreply.github.com>
2026-03-07 22:21:26 +0100

    debugggg

--- src/transformers/utils/import_utils.py
+++ src/transformers/utils/import_utils.py
@@ -2119,6 +2119,9 @@ class _LazyModule(ModuleType):
 
             Placeholder.__module__ = module_name
 
+            print(f"f{name}")
+            print(f"f{module_name}")
+            print("oh la la!!!!")
             value = Placeholder
         elif name in self._class_to_module:
             try:
